d92087f90c64ebf7029a4726b6e34c9d50e8f42e
rush <rushastronomo0415@gmail.com>
2019-07-05 17:25:09 -0700

    updated components

--- src/js/components/History/History.jsx
+++ src/js/components/History/History.jsx
@@ -1,28 +1,3 @@
-// export default class History extends React.Component {
-//   constructor(props) {
-//     super(props);
-//   }
-
-//   render() {
-//     // These values were provided by connect()
-//     const { history } = this.props;
-//     return (
-//       <div>
-//         <h5 className='card-header text-white bg-primary mb-3'>Search History</h5>
-//         <div className='card-body'>
-//           <ul className='list-unstyled'>
-//             { history.map((histories, index) => (
-//               <li key={ index }>
-//                 <p>{ histories.cityInput } - { histories.date }</p>
-//               </li>
-//               )
-//             )}
-//           </ul>
-//         </div>
-//       </div>
-//     );
-//   }
-// }
 import React, { Component } from 'react';
 
 export default class History extends Component {
