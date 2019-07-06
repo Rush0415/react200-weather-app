21bb78917dfaa69a2b6f4cdb5573b8fb7add23a2
rush <rushastronomo0415@gmail.com>
2019-07-05 17:12:03 -0700

    updated components

--- src/js/components/History/index.js
+++ src/js/components/History/index.js
@@ -1,14 +1,3 @@
-// import { connect } from 'react-redux';
-// import History from './History';
-
-// function mapStoreToProps(store) {
-//   return {
-//     cityInput: store.weather.cityInput,
-//     history: store.weather.history
-//   };
-// }
-
-// export default connect(mapStoreToProps)(History);
 import { connect } from 'react-redux';
 import History from './History';
 
--- src/js/components/Search/index.js
+++ src/js/components/Search/index.js
@@ -1,10 +1,3 @@
-// import { connect } from 'react-redux';
-// import Search from './Search';
-
-// const mapStoreToProps = store => ({
-//   city: store.weather.city
-// });
-// export default connect(mapStoreToProps)(Search);
 import { connect } from 'react-redux';
 import Search from './Search';
 
