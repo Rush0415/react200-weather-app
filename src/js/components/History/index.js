b3720c8d31b55ad88616143344c889efb1e16daf
rush <rushastronomo0415@gmail.com>
2019-07-07 00:58:03 -0700

    added .circleci and circle.yml

--- src/js/components/History/index.js
+++ src/js/components/History/index.js
@@ -1,15 +1,3 @@
-// import { connect } from 'react-redux';
-// import SearchHistory from './history';
-
-// function mapStoreToProps(store) {
-//   return {
-//     history: store.search.history
-//   };
-// }
-
-// export default connect(mapStoreToProps)(SearchHistory);
-
-
 import { connect } from 'react-redux';
 import HistoryData from './History';
 
